b370e87bd1cc21f167d4010305d5d3ed50e95999
arjunshetty1 <arjunjayapura@mail.com>
2024-03-07 15:48:23 +0530

    Link add

--- routes/index.js
+++ routes/index.js
@@ -9,20 +9,23 @@ router.use(bodyParser.json({ limit: "100mb" }));
 router.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
 const UPLOAD_DIR = path.join(__dirname, "uploads"); // Define the upload directory
 
-router.post("http://localhost:3002/receive-image", (req, res) => {
-  try {
-    console.log(req.body.image);
-    const imageData = Buffer.from(req.body.image); // Extract image data from request body
-    const imageName = req.body.imageName; // Define the image name
-    const imagePath = path.join(UPLOAD_DIR, imageName); // Construct the full image path
+router.post(
+  "https://edueval-pyserver-o0my6mg1o-arjun-shetty.vercel.app/receive-image",
+  (req, res) => {
+    try {
+      console.log(req.body.image);
+      const imageData = Buffer.from(req.body.image); // Extract image data from request body
+      const imageName = req.body.imageName; // Define the image name
+      const imagePath = path.join(UPLOAD_DIR, imageName); // Construct the full image path
 
-    fs.writeFileSync(imagePath, imageData); // Save the received image to the uploads directory
-    console.log("Image received and saved successfully");
-    res.send("Image received and saved");
-  } catch (error) {
-    console.error("Error receiving image:", error.message);
-    res.status(500).send("Error receiving image");
+      fs.writeFileSync(imagePath, imageData); // Save the received image to the uploads directory
+      console.log("Image received and saved successfully");
+      res.send("Image received and saved");
+    } catch (error) {
+      console.error("Error receiving image:", error.message);
+      res.status(500).send("Error receiving image");
+    }
   }
-});
+);
 
 module.exports = router;
