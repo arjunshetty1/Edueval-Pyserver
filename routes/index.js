8f35d92d594d8ba12865b955a6c30a7dac82d9c9
arjunshetty1 <arjunjayapura@mail.com>
2024-03-14 01:50:55 +0530

    Vison API added

--- routes/index.js
+++ routes/index.js
@@ -7,22 +7,20 @@ router.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
 
 router.post("/receive-image", (req, res) => {
   try {
-    const imageData = req.body.image; // Extract image buffer from request body
-    const imageName = req.body.imageName; // Extract image name from request body
+    const image = req.body.image;
     const question = req.body.question; // Extract question from request body
     const answerkey = req.body.answerkey; // Extract answer key from request body
 
     // Log the received data
-    console.log("Image:", imageName);
+    console.log("Image:", image);
     console.log("Question:", question);
     console.log("Answer Key:", answerkey);
-    console.log("Image Buffer:", imageData);
 
     console.log("Data received and logged successfully");
-    res.send("Data received and logged");
+    res.send(image);
   } catch (error) {
-    console.error("Error receiving data:", error.message);
-    res.status(500).send("Error receiving data");
+    console.error("Error receiving data in server 2 :", error.message);
+    res.status(500).send("Error receiving data in server 2");
   }
 });
 
